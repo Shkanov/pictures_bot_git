1824cffe2e6426737a38677f898aa619efd55893
shkanov <bulat-steel@mail.ru>
2018-03-21 00:48:24 +0400

    no stop

--- pictures_bot.py
+++ pictures_bot.py
@@ -256,4 +256,18 @@ def query_text(query):
 """
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    try:
+
+        bot.polling(none_stop=True)
+
+        # ConnectionError and ReadTimeout because of possible timout of the requests library
+
+        # TypeError for moviepy errors
+
+        # maybe there are others, therefore Exception
+
+    except Exception as e:
+
+        logger.error(e)
+
+        time.sleep(15)
